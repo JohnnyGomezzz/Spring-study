809d76da579cb7108bb4e12d82ec63defe59e244
JohnnyGomezzz <seldesign@mail.ru>
2021-04-28 14:49:20 +0300

    hw6

---
+++ src/main/java/ru/johnnygomezzz/controllers/ProductController.java
@@ -0,0 +1,70 @@
+package ru.johnnygomezzz.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.johnnygomezzz.models.Product;
+import ru.johnnygomezzz.services.ProductService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class ProductController {
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String showAllProductsPage(Model model) {
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "index";
+    }
+
+    @GetMapping("/products/{id}")
+    public String showProductInfo(@PathVariable(name = "id") Long id, Model model) {
+        Optional<Product> product = productService.findOneById(id);
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+        }
+        return "product_info";
+    }
+    @PostMapping("/products/{id}")
+    public String searchStudentInfo(@RequestParam(defaultValue = "0") Long id, Model model) {
+        productService.findOneById(id).ifPresent(p -> model.addAttribute("product", p));
+        return "product_info";
+    }
+
+    @GetMapping("/products/add")
+    public String showCreator() {
+        return "add_product_form";
+    }
+    @PostMapping("/products/add")
+    public String createNewProduct(@ModelAttribute Product product) {
+        productService.save(product);
+        return "redirect:/";
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public String deleteProductById(@PathVariable Long id) {
+        productService.deleteById(id);
+        return "redirect:/";
+    }
+
+//    @GetMapping("/students/increase/{id}")
+//    public String increaseStudentScoreById(@PathVariable Long id) {
+//        productService.increaseScore(id);
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/students/decrease/{id}")
+//    public String decreaseStudentScoreById(@PathVariable Long id) {
+//        productService.decreaseScore(id);
+//        return "redirect:/";
+//    }
+}
